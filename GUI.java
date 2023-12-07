26e478c0fe9a8e58f00d51b772e1284bab850d1e
Kash Torino <148072181+eimryst@users.noreply.github.com>
2023-12-07 23:03:58 +0800

    Update GUI.java
    removed build failed

--- GUI.java
+++ GUI.java
@@ -3,17 +3,9 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
-import javax.swing.SwingConstants;
-import javax.swing.ButtonGroup;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JRadioButton;
-import javax.swing.JTextField;
-import java.awt.Font;
+import javax.swing.*;
 
-public class GUI implements ActionListener {
+public class GUI {
 
     private static ExpenseTracker expenseTracker = new ExpenseTracker();
 
